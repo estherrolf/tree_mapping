91802debd9c970588b994f87b40269660ebf74a5
Esther Rolf <erolf@seas.harvard.edu>
2023-12-01 17:23:43 -0500

    saving tifs from predicted model output and evaluate performance

--- model_to_tif.py
+++ model_to_tif.py
@@ -1,16 +1,18 @@
-import torch
-import pixelwise_regression_task_with_mask
-import rasterio
-import tqdm 
+import argparse
 import numpy as np
 import os
+import rasterio
+import torch
 from torch.utils.data import DataLoader
 from torchgeo.datasets import RasterDataset, Sentinel2, stack_samples, AsterGDEM, IntersectionDataset
 from torchgeo.samplers import GridGeoSampler
+from trainers.regression_with_nans import PixelwiseRegressionTask
+import tqdm 
+import yaml
 
-from chm_datamodule import ChmDataModule, transforms_4_channel_rgbnir_plus_mask_imagestats, transforms_4_channel_rgbnir_imagestats, make_site_dataset
-
-import matplotlib.pyplot as plt 
+from datamodules.chm_datamodule import ChmDataModule, make_site_dataset, transforms_4_channel_rgbnir_no_mask_imagestats
+from eval_utils import get_lowest_val_checkpoint, init_model_from_checkpoint
+from experiment_utils import get_site_splits
 
 sentinel_layer_codes = {"b": "B02",
                         "g": "B03",
@@ -19,56 +21,71 @@ sentinel_layer_codes = {"b": "B02",
                         "vis":"TCI",
                        }
 
+def read_config_file(config_yaml):
+    with open(config_yaml, "r") as cfg_file:
+        cfg = yaml.safe_load(cfg_file)
+    return cfg
+
+def load_ckpt_weight_to_model(task_conditions_dict, 
+                      checkpoint_dir
+                      ):
+
+    lowest_val_ckpt_fp = get_lowest_val_checkpoint(checkpoint_dir)
+    
+    model = PixelwiseRegressionTask(**task_conditions_dict).model
+
+    init_model_from_checkpoint(model, lowest_val_ckpt_fp)
+    return model, lowest_val_ckpt_fp
+
+
 # predict and save as tif for a given site
-def predict_site(eval_input_dir, 
-                 exp_dir,
-                 model_id,
-                 model_conditions_dict,
+def predict_site(site_id, 
+                 checkpoint_dir,
+                 task_conditions_dict,
+                 img_layers,
                  output_fp,
+                 transforms_pred_model,
                  pred_args = {},
-                 transforms_pred_model = transforms_4_channel_rgbnir_plus_mask_imagestats,
                  nodata_value = -9999.,
                  img_nodata_value = -9999.,
                  nodata_pad= 5,
-                 device='gpu',
-                 model_suffix='train_sites_17_de_de_08_01_06_15',
-                 path_to_data='data'
+                 device='cuda',
+                 data_dir='data',
                 ):
     """
     nodata_pad will pad any predictions within that many pixels of a nodata input image as nodata output.
     """
     
-    # read the relavant filenames
     # load the model for evaluation
-    model_dir = f"{exp_dir}/models"
-    print(model_suffix)
-    task_eval, model_fp = load_model_to_task(model_conditions_dict, model_dir, model_suffix=model_suffix)
-    task_eval = task_eval
-    
-    return predict_site_with_model(task_eval, 
+    model, lowest_val_ckpt_fp = load_ckpt_weight_to_model(
+        task_conditions_dict, 
+        checkpoint_dir
+    )
+    model = model.to(device).eval() 
+    return predict_site_with_model(site_id,
+                                   model, 
                                    output_fp,
-                                   pred_arg=pred_args, 
-                                   transforms_pred_model = transforms_4_channel_rgbnir_plus_mask_imagestats,
+                                   img_layers,
+                                   pred_args=pred_args, 
+                                   transforms = transforms_pred_model,
                                    nodata_value = nodata_value,
                                    img_nodata_value = img_nodata_value,
                                    nodata_pad= nodata_pad,
                                    device=device,
-                                   path_to_data=path_to_data)
+                                   data_dir=data_dir)
 
     
 def predict_site_with_model(site_id, 
                             model,
                             output_fp,
+                            img_layers,
+                            transforms,
                             pred_args = {},
-                  #          needs_dem=False,
-                            context_model=False,
-                            num_contexts=2,
                             nodata_value = -9999.,
                             img_nodata_value = -9999.,
                             nodata_pad= 5,
-                            device='gpu',
-                            model_suffix='train_sites_17_de_de_08_01_06_15',
-                            path_to_data='data'
+                            device='cuda',
+                            data_dir='data'
                             ):
 
     
@@ -76,26 +93,8 @@ def predict_site_with_model(site_id,
     pad = pred_args['padding']
     
     dataset = make_site_dataset(site_id, 
-                                layers=['r','g','b','nir','vis', 'canopy'], # no CHM
-                                transforms=transforms_4_channel_rgbnir_imagestats)
-
-#     get the input image file for evaluation
-#     if needs_dem:
-#         dem_data_dir = f'{path_to_data}/dem'
-#         def aster_transform(sample):
-#             sample['dem'] = sample['mask'].clone()
-#             return sample
-
-#         dem = AsterGDEM(root=dem_data_dir, transforms=aster_transform)
-
-
-#         s2_dataset = Single4ChannelSentinel2Dataset(eval_input_dir, 
-#                                                     transforms=None)
-#         s2_dataset = IntersectionDataset(s2_dataset, dem, transforms=transforms_4_channel_rgbnir_plus_mask_imagestats)
-        
- #   canopy_dataset = RasterDataset(root=os.path.join(data_dir, canopy_relative_dir, site_id))
-    # s2_dataset = Single4ChannelSentinel2Dataset(eval_input_dir, 
-    #                                                 transforms=transforms_4_channel_rgbnir_plus_mask_imagestats)
+                                layers=img_layers, # no CHM
+                                transforms=transforms)
 
     sampler = GridGeoSampler(dataset, pred_args['patch_size'], stride=pred_args['stride'])
 
@@ -117,9 +116,6 @@ def predict_site_with_model(site_id,
             transform = profile["transform"]
     output = np.ones((input_height, input_width), dtype=np.float32) * nodata_value
     
-    if context_model:
-        output_context = np.ones((num_contexts, input_height, input_width), dtype=np.float32) * nodata_value
-
     dl_enumerator = tqdm.tqdm(dataloader)
 
     # modification of code from Caleb
@@ -128,23 +124,10 @@ def predict_site_with_model(site_id,
             bboxes = batch["bbox"]
             with torch.inference_mode():
                 
-                if context_model:
-                    context = batch["context"].to(device)
-                    
-                    (predictions, y_hat_by_context), context = model(images, context)
-                    
-#                     if model.context_integrator_type in ['weighted_avg','attn']:
-#                         predictions, context = model(images, context)
-#                     elif model.context_integrator_type in ['separated_by_context']:
-                        
-#                     else:
-#                         print(f"context integrator type {model.context_integrator_type} not recognized")
-                else:
-                    predictions = model(images)
+                predictions = model(images)
 
                 predictions = predictions[:,0].cpu().numpy()
-                if context_model:
-                    context = context.cpu().numpy()
+
                 #anything within pad pixels of a nodata img pixels gets a nodata label
                 img_nodata_ = (images == img_nodata_value).any(dim=1).cpu().numpy().astype(float)
                 
@@ -153,10 +136,6 @@ def predict_site_with_model(site_id,
             
                 img_nodata_padded = padder(torch.Tensor(img_nodata_)).cpu().numpy()
                 predictions[:,nodata_pad:-nodata_pad, nodata_pad:-nodata_pad][img_nodata_padded > 1 ] = nodata_value
-                
-                if context_model:
-                    for c in range(num_contexts):
-                        context[:,c,nodata_pad:-nodata_pad, nodata_pad:-nodata_pad][img_nodata_padded > 1 ] = nodata_value
 
                 
             for i in range(len(bboxes)):
@@ -166,15 +145,29 @@ def predict_site_with_model(site_id,
                 right, bottom = ~transform * (bb.maxx, bb.miny)
                 left, right, top, bottom = int(np.round(left)), int(np.round(right)), int(np.round(top)), int(np.round(bottom))
 
-                assert right - left == pred_args['patch_size']
-                assert bottom - top == pred_args['patch_size']
+                patch_size = pred_args['patch_size']
+                tile_size = patch_size - 2*pad
+                assert right - left == patch_size
+                assert bottom - top == patch_size
 
+                x1, y1 = top+pad, left+pad
+            
+                # fix if at ends
+                pred_tile = predictions[i]
+
+                if x1 < 0:
+                    width = patch_size+x1
+                    pred_tile = pred_tile[-(width):,:]
+                if y1 + tile_size > output.shape[1]:                
+                    width = output.shape[1] - y1 + 2*pad
+                    pred_tile = pred_tile[:,:width]
+
+                x2 = x1 + tile_size
+                y2 = y1 + tile_size
+                if x1 < 0: x1 = 0
                 
-                # image indexed so in integegers top < bottom
-                output[top+pad:bottom-pad, left+pad:right-pad] = predictions[i][pad:-pad, pad:-pad]
-                
-                if context_model:
-                    output_context[:,top+pad:bottom-pad, left+pad:right-pad] = context[i][:,pad:-pad, pad:-pad]
+                output[x1:x2,y1:y2] = pred_tile[pad:-pad, pad:-pad]
+
 
                     
     #save the output
@@ -186,51 +179,95 @@ def predict_site_with_model(site_id,
         with rasterio.open(output_fp, 'w',**profile_out) as dst:
             dst.write(output, 1)
             
-        if context_model:
-            profile_out.update(dtype='float32', count=num_contexts)
-            with rasterio.open(output_fp.replace('.tif', '_context.tif'), 'w',**profile_out) as dst:
-                dst.write(output_context)
-        
     return output
 
-    
-                    
-class Single4ChannelSentinel2Dataset(Sentinel2):
-    
-    img_layers = ['r','g','b','nir','vis']
+            
+def make_model_output_dirs(exp_name, seed, split_number):
+    out_dir = 'predicted_maps'
+    out_dir_exp = os.path.join(out_dir, exp_name)
+    out_dir_split = os.path.join(out_dir, exp_name, f'seed_{split_seed}_split_{split_number}')
+    for path_name in [out_dir, out_dir_exp, out_dir_split]:
+        if not os.path.exists(path_name): os.mkdir(path_name)
         
-    bands = [sentinel_layer_codes[l.lower()] for l in img_layers]
-    
-    def __init__(self, root_dir, transforms):
-        super().__init__(root=root_dir, 
-                         bands=self.bands,
-                         transforms=transforms)
-        
-        
-def load_model_to_task(conditions_dict, 
-                       model_dir, 
-                       model_type='fcn',
-                       model_suffix='train_sites_17_de_de_08_01_06_15'
-                      ):
-    if model_type != 'fcn': 
-        print('only using FCN for now')
-        return
-    
-    lr, num_filters = conditions_dict['lr'], conditions_dict['num_filters']
-    
-    this_model_fn = f"filters_{num_filters}_lr_{lr}_{model_suffix}.fcn"
+    return out_dir_split
 
-    model_load_path = f"{model_dir}/{this_model_fn}"
+def run_experiment_models_through_one_split(
+    test_sites, 
+    checkpoint_dir,
+    out_dir_split,
+    version_id, 
+    experiment_cfg,
+    device='cuda',
     
-    task = pixelwise_regression_task_with_mask.PixelwiseRegressionTaskWithMask(model='fcn', 
-                                   loss='mse',
-                                   learning_rate_schedule_patience=10,
-                                   weights=None,
-                                   in_channels=4,
-                                   num_classes=1, 
-                                   **conditions_dict
-                            )
+):
+    
+    num_image_channels = cfg['data']['num_image_channels']
+    if num_image_channels == 4:
+        img_layers = ['r','g','b','nir','vis']
+        transforms_pred_model=transforms_4_channel_rgbnir_no_mask_imagestats
+    else:
+        print('no directive for {num_image_channels} image channels')
+    
+    pred_args = {
+        'patch_size': 64, 
+        'padding': 5,
+        'batch_size': 1,
+        'num_workers': 1,
+        'stride': 54,
+    }
 
-    task.model.load_state_dict(torch.load(model_load_path))
+    task_conditions_dict = experiment_cfg['task']
+    nodata_pad = task_conditions_dict['pad_pixels']
     
-    return task, model_load_path
+
+    for site_id in test_sites:
+        output_dir = os.path.join(out_dir_split, site_id)
+        if not os.path.exists(output_dir): os.mkdir(output_dir)
+        output_fp = os.path.join(output_dir, f'preds_{site_id}_{version_id}.tif')
+
+
+        output =  predict_site(site_id, 
+                               checkpoint_dir,
+                               task_conditions_dict,
+                               img_layers=img_layers,
+                               output_fp=output_fp,
+                               transforms_pred_model=transforms_pred_model,
+                               pred_args = pred_args,
+                               nodata_value = -9999.,
+                               img_nodata_value = -9999.,
+                               nodata_pad= 5,
+                               device=device,
+                        )
+    
+if __name__ == "__main__":
+    torch.set_float32_matmul_precision('high')
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_fp")
+    args = parser.parse_args()
+    config_fp = args.config_fp
+    #config_fp = 'experiment_configs/train_baseline_local_models.yaml'
+    cfg = read_config_file(config_fp)
+    
+    base_name = cfg['base_name']
+    exp_name = cfg['exp_name']
+    version_id_base = cfg['version_id_base']
+    split_seed = cfg['data']['split_seed']
+    
+    
+    for split_number in range(1):
+        version_id = f'{version_id_base}_split_{split_number}_seed_{split_seed}'
+        exp_root_dir = os.path.join(base_name,exp_name)
+        checkpoint_dir = os.path.join(exp_root_dir,  'models', version_id)
+        out_dir_split = make_model_output_dirs(exp_name, split_seed, split_number)
+        
+        test_sites = get_site_splits(split_seed)[split_number]["test_sites"]
+    
+        run_experiment_models_through_one_split(
+            test_sites,
+            checkpoint_dir,
+            out_dir_split,
+            version_id, 
+            cfg,
+        )
+        
