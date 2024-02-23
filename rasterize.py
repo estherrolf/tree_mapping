61e17c57684b539580f92cb4529dba7eb3cfe120
Lucia Gordon <luciagordon@g.harvard.edu>
2024-02-23 13:47:06 -0500

    Changed path and added if name equals main

--- rasterize.py
+++ rasterize.py
@@ -1,14 +1,19 @@
 # imports
-from osgeo import gdal
 from rasterio import features
+from utils import get_project_dir
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import numpy as np
 import rasterio
 
-gdal.UseExceptions()
+data_dir = f'{get_project_dir()}/data'
 
-def rasterize(vector_path, raster_name, resolution=10): # resolution = number of meters covered by each pixel in the TIFF
+def rasterize(vector_path, resolution): # resolution = number of meters covered by each pixel in the TIFF
+    '''Converts a vector to a raster at a specified resolution'''
+
+    feature = vector_path.split('/')[-1].split('_')[0]
+    raster_path = f'{data_dir}/features/{feature}/{feature}_raster_{resolution}m.tif'
+
     # rasterize shapefile
     vector = gpd.read_file(vector_path)
     bounds = vector.total_bounds
@@ -16,8 +21,7 @@ def rasterize(vector_path, raster_name, resolution=10): # resolution = number of
     height = int((bounds[3] - bounds[1]) / resolution)
     transform = rasterio.transform.from_origin(bounds[0], bounds[3], resolution, resolution)
 
-    out_tiff = f'{raster_name}.tiff'
-    with rasterio.open(out_tiff,
+    with rasterio.open(raster_path,
                        'w',
                        driver='GTiff',
                        height=height,
@@ -26,23 +30,22 @@ def rasterize(vector_path, raster_name, resolution=10): # resolution = number of
                        dtype=rasterio.uint8,
                        crs=vector.crs,
                        transform=transform) as dst:
+
         burned = features.rasterize(((geometry, 255) for geometry in vector.geometry),
                                      out_shape=(height, width),
                                      transform=transform,
                                      fill=0,
                                      all_touched=True,
                                      dtype=rasterio.uint8) # burn the features into the raster
-        
+
         dst.write_band(1, burned) # write the rasterized shapefile to the GeoTIFF
 
     # convert raster to array
-    raster = gdal.Open(f'{raster_name}.tiff')
-    num_rows = raster.RasterYSize
-    num_cols = raster.RasterXSize
-    array = ((raster.GetRasterBand(1)).ReadAsArray(0, 0, num_cols, num_rows).astype(np.float32)) # 0 = not river, 255 = river
+    array = rasterio.open(raster_path).read(1) # 0 = not river, 255 = river
     print(f'Array shape = {array.shape}')
     print(f'Min array = {np.amin(array)}, max array = {np.amax(array)}')
 
+    # check that all values are 0 or 255
     for row in range(array.shape[0]):
         for col in range(array.shape[1]):
             if array[row][col] != 0 and array[row][col] != 255:
@@ -51,9 +54,12 @@ def rasterize(vector_path, raster_name, resolution=10): # resolution = number of
     # plot raster
     plt.figure(dpi=300)
     plt.imshow(array) # plot the array of pixel values as an image
-    plt.axis('off') # remove axes        
-    plt.savefig(f'{raster_name}.png', bbox_inches='tight', pad_inches=0)
+    plt.axis('off') # remove axes
+    plt.savefig(f'{feature}_raster_{resolution}m.png', bbox_inches='tight', pad_inches=0)
     plt.close() # close the image to save memory
 
-rasterize(vector_path='../river-shapefile/river-shapefile.shp',
-          raster_name='river_raster')
+    print(f'Rasterized at {resolution}m resolution')
+
+if __name__ == '__main__':
+    rasterize(vector_path=f'{data_dir}/features/river/river_shapefile/river_shapefile.shp', resolution=10)
+    rasterize(vector_path=f'{data_dir}/features/river/river_shapefile/river_shapefile.shp', resolution=30)
