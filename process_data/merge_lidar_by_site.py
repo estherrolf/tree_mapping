a514a455f3e75933589a2f0d43f85945fa99ef92
Lucia Gordon <luciagordon@g.harvard.edu>
2024-03-07 01:13:55 -0500

    Simplified coarsening procedure

--- process_data/merge_lidar_by_site.py
+++ process_data/merge_lidar_by_site.py
@@ -1,5 +1,6 @@
 # imports
 from osgeo import gdal
+import math
 import numpy as np
 import os
 import rasterio
@@ -35,77 +36,41 @@ def preprocess_lidar_1m_tiffs():
     os.makedirs(lidar_1m_dir, exist_ok=True) # make 1m lidar directory
 
     for tiff_path in lidar_1m_merged_dir_tiffs:
-        tiff = rasterio.open(f'{lidar_1m_merged_dir}/{tiff_path}')
-        metadata = tiff.meta
-        tiff_array = tiff.read()
-        tiff_array[(tiff_array < 0) & (tiff_array != NAN)] = 0 # sets non-NaN negative values to 0
-        tiff_array[tiff_array > 30] = NAN # sets values > 30 to the no-data value
+        with rasterio.open(f'{lidar_1m_merged_dir}/{tiff_path}') as tiff:
+            metadata = tiff.meta
+            tiff_array = tiff.read()
+            tiff_array[(tiff_array < 0) & (tiff_array != NAN)] = 0 # sets non-NaN negative values to 0
+            tiff_array[tiff_array > 30] = NAN # sets values > 30 to the no-data value
 
-        with rasterio.open(f'{lidar_1m_dir}/{tiff_path}', 'w', **metadata) as out_file: # replaces the original tiff with a new tiff with the same metadata, just different values
-            out_file.write(tiff_array)
-
-def pad_array(array, pad_value, row_divisor, column_divisor):
-    '''Pads an array by adding rows and/or columns filled with a constant value to the bottom and/or right of the array'''
-
-    num_input_rows, num_input_cols = array.shape
-    num_rows_to_append = int((row_divisor - num_input_rows%row_divisor) % row_divisor) # divisible by row divisor
-    num_cols_to_append = int((column_divisor - num_input_cols%column_divisor) % column_divisor) # divisible by column divisor
-    rows_to_append = np.full((num_rows_to_append, num_input_cols), pad_value)
-    padded_array = np.vstack((array, rows_to_append)) # adds rows to the bottom
-    cols_to_append = np.full((padded_array.shape[0], num_cols_to_append), pad_value)
-    padded_array = np.hstack((padded_array, cols_to_append)) # adds columns to the right
-
-    return padded_array
-
-def calculate_percentile(array, percentile):
-    '''Calculates the percentile of the non-NaN values of an array according to the linear interpolation method'''
-
-    array = np.array(array) # converts array to a numpy array if it is not already
-    array_without_nans = array[array != NAN] # 1D array of non-NaN values
-
-    if len(array_without_nans) == 0: # if the original array contains all NaNs
-        return NAN
-
-    sorted_array = sorted(array_without_nans)
-    index = percentile/100 * (len(sorted_array) - 1)
-
-    if index.is_integer():
-        return sorted_array[int(index)]
-
-    # linear interpolation between the lower and upper indices is performed if the index is not an integer
-    lower_index = int(index)
-    upper_index = lower_index + 1
-    interpolation = index - lower_index
-    percentile = sorted_array[lower_index] + interpolation * (sorted_array[upper_index] - sorted_array[lower_index])
-
-    return percentile
+            with rasterio.open(f'{lidar_1m_dir}/{tiff_path}', 'w', **metadata) as out_file: # replaces the original tiff with a new tiff with the same metadata, just different values
+                out_file.write(tiff_array)
 
 def coarsen_tiff(output_path, input_path, target_resolution):
     '''Coarsens an input tiff to a target resolution and saves it'''
 
-    input_tiff = rasterio.open(input_path)
-    input_tiff_array = input_tiff.read(1) # extracts the first band of the tiff as an array
-    input_resolution = input_tiff.res[0] # meters per pixel
-    input_tiff_metadata = input_tiff.meta # dictionary of driver, dtype, nodata, width, height, count, crs, & transform
-    factor = int(target_resolution / input_resolution) # downsampling factor
-    print(f'Input tiff shape = {input_tiff_array.shape}')
-    padded_tiff_array = pad_array(array=input_tiff_array, pad_value=NAN, row_divisor=target_resolution, column_divisor=factor) # pads the input tiff so that the numbers of rows and columns are divisible by the downsampling factor
-    num_padded_tiff_rows, num_padded_tiff_cols = padded_tiff_array.shape
-    print(f'Padded tiff shape = {num_padded_tiff_rows, num_padded_tiff_cols}')
-    coarsened_tiff_array = np.full((int(num_padded_tiff_rows / factor), int(num_padded_tiff_cols / factor)), NAN) # initializes an array filled with NaNs of the proper size for the coarsened tiff
+    with rasterio.open(input_path) as input_tiff:
+        input_tiff_array = input_tiff.read(1) # extracts the first band of the tiff as an array
+        num_input_tiff_rows, num_input_tiff_cols = input_tiff.shape
+        input_resolution = input_tiff.res[0] # meters per pixel
+        input_tiff_metadata = input_tiff.meta # dictionary of driver, dtype, nodata, width, height, count, crs, & transform
+        factor = int(target_resolution / input_resolution) # downsampling factor
+        coarsened_tiff_array = np.full((int(math.ceil(num_input_tiff_rows/factor)), int(math.ceil(num_input_tiff_cols/factor))), NAN) # initializes an array filled with NaNs of the proper size for the coarsened tiff
+        print(f'Input tiff shape = {input_tiff_array.shape}')
 
-    for top in range(0, num_padded_tiff_rows, factor): # begins from the top of the padded input array
-        for left in range(0, num_padded_tiff_cols, factor): # begins from the left end of the padded input array
-            coarsened_tiff_array[int(top / factor), int(left / factor)] = calculate_percentile(array=padded_tiff_array[top : top+factor, left : left+factor], percentile=90) # sets a pixel in the coarsened tiff array to be the 90th percentile of a factor x factor sub-array in the padded input tiff
+        for top in range(0, num_input_tiff_rows, factor): # begins from the top of the padded input array
+            for left in range(0, num_input_tiff_cols, factor): # begins from the left end of the padded input array
+                block = input_tiff_array[top : top+factor, left : left+factor].copy()
+                block_without_nans = block[block != NAN]
+                coarsened_tiff_array[int(top / factor), int(left / factor)] = NAN if len(block_without_nans) == 0 else np.percentile(a=block_without_nans, q=90) # sets a pixel in the coarsened tiff array to be the 90th percentile of a sub-array in the input tiff
+        
+        print(f'Coarsened tiff shape = {coarsened_tiff_array.shape}')
+        coarsened_tiff_metadata = input_tiff_metadata.copy()
+        coarsened_tiff_metadata['height'] = coarsened_tiff_array.shape[0] # sets the height of the coarsened tiff to the number of rows in its array
+        coarsened_tiff_metadata['width'] = coarsened_tiff_array.shape[1] # sets the width of the coarsened tiff to the number of columns in its array
+        coarsened_tiff_metadata['transform'] = rasterio.transform.Affine(factor*input_tiff.transform[0], input_tiff.transform[1], input_tiff.transform[2], input_tiff.transform[3], factor*input_tiff.transform[4], input_tiff.transform[5]) # sets the x- and y-resolutions of the coarsened tiff
 
-    print(f'Coarsened tiff shape = {coarsened_tiff_array.shape}')
-    coarsened_tiff_metadata = input_tiff_metadata.copy()
-    coarsened_tiff_metadata['height'] = coarsened_tiff_array.shape[0] # sets the height of the coarsened tiff to the number of rows in its array
-    coarsened_tiff_metadata['width'] = coarsened_tiff_array.shape[1] # sets the width of the coarsened tiff to the number of columns in its array
-    coarsened_tiff_metadata['transform'] = rasterio.transform.Affine(factor*input_tiff.transform[0], input_tiff.transform[1], input_tiff.transform[2], input_tiff.transform[3], factor*input_tiff.transform[4], input_tiff.transform[5]) # sets the x- and y-resolutions of the coarsened tiff
-
-    with rasterio.open(output_path, 'w', **coarsened_tiff_metadata) as out_file:
-        out_file.write(coarsened_tiff_array, indexes=1) # saves the coarsened tiff
+        with rasterio.open(output_path, 'w', **coarsened_tiff_metadata) as out_file:
+            out_file.write(coarsened_tiff_array, indexes=1) # saves the coarsened tiff
 
 def coarsen_lidar_1m_tiffs(target_resolution):
     '''Coarsens the 1m-resolution lidar tiff for each site to the target resolution'''
