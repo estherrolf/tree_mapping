e83dbff5264b1ba7bec5a0e08a71a80eea2fe981
Lucia Gordon <luciagordon@g.harvard.edu>
2024-03-12 10:17:12 -0400

    Increased job time

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1                # Number of cores
 #SBATCH -N 1                # Ensure that all cores are on one machine
 #SBATCH -p gpu,tambe_gpu
-#SBATCH -t 0-3:00:0         # Runtime in D-HH:MM:SS, minimum of 10 minutes
+#SBATCH -t 0-4:00:0         # Runtime in D-HH:MM:SS, minimum of 10 minutes
 #SBATCH --mem 32G          # Memory pool for all cores (see also --mem-per-cpu) MBs
 #SBATCH --cpus-per-task 24
 #SBATCH --gres gpu:1
