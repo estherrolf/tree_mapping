b1a5626d1fed37c759999011a0ce826d71144b44
Lucia Gordon <luciagordon@g.harvard.edu>
2024-04-06 12:42:44 -0400

    Used argparse

--- train_models.py
+++ train_models.py
@@ -5,10 +5,10 @@ from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
 from lightning.pytorch.loggers import TensorBoardLogger
 from lightning.pytorch import Trainer
 from trainers.regression_with_nans import PixelwiseRegressionTask
-from utils import get_project_dir
+from utils import get_project_dir, str_to_bool
+import argparse
 import itertools
 import os
-import sys
 import torch
 import yaml
 
@@ -29,9 +29,17 @@ def setup_chm_datamodule(sites_per_split, cfg_data):
     
     return chm
 
-def train(datamodule, task, base_name, exp_name, version_id_base, num_channels, split_number, version_id, **trainer_kwargs):
+def train(datamodule, task, base_name, exp_name, version_id_base, split_number, version_id, num_channels=None, layers_tuned=None, freeze_backbone=None, **trainer_kwargs):
     # set up log dirs
-    exp_root_dir = f'{get_project_dir()}/{base_name}/{exp_name}/{version_id_base}/{num_channels}_channels/split_{split_number}'
+    if num_channels is not None:
+        exp_root_dir = f'{get_project_dir()}/{base_name}/{exp_name}/{version_id_base}/{num_channels}_channels/split_{split_number}'
+    elif layers_tuned is not None:
+        exp_root_dir = f'{get_project_dir()}/{base_name}/{exp_name}/{version_id_base}/{layers_tuned}_layers_tuned/split_{split_number}'
+    elif freeze_backbone is not None:
+        exp_root_dir = f'{get_project_dir()}/{base_name}/{exp_name}/{version_id_base}/freeze_backbone_{freeze_backbone}/split_{split_number}'
+    else:
+        exp_root_dir = f'{get_project_dir()}/{base_name}/{exp_name}/{version_id_base}/split_{split_number}'
+
     os.makedirs(f'{exp_root_dir}/logs', exist_ok=True)
     os.makedirs(f'{exp_root_dir}/models', exist_ok=True)
     
@@ -54,7 +62,7 @@ def train(datamodule, task, base_name, exp_name, version_id_base, num_channels, 
     print('datamodule', datamodule)
     trainer.fit(model=task, datamodule=datamodule)
     
-def run_experiment(config_fp, lr=None, weight_decay=None, channels=None):
+def run_experiment(config_fp, lr, weight_decay, channels, layers_tuned, freeze_backbone):
     cfg = read_config_file(config_fp)
 
     # get this data split
@@ -76,20 +84,29 @@ def run_experiment(config_fp, lr=None, weight_decay=None, channels=None):
         if channels is not None:
             task['in_channels'] = channels
             cfg['data']['num_image_channels'] = channels
+        if layers_tuned is not None:
+            task['num_layers_to_unfreeze'] = layers_tuned
+        if freeze_backbone is not None:
+            task['freeze_backbone'] = freeze_backbone
         
         # get the assignment for this split number
         sites_per_split = splits[split_number]
         chm = setup_chm_datamodule(sites_per_split, cfg['data'])
         chm_task = PixelwiseRegressionTask(**task)
         version_id = f'lr_{task["lr"]}_wd_{task["weight_decay"]}'
-        train(chm, chm_task, base_name, exp_name, version_id_base, task['in_channels'], split_number, version_id, **cfg['trainer'])
+
+        train(chm, chm_task, base_name, exp_name, version_id_base, split_number, version_id, channels, layers_tuned, freeze_backbone, **cfg['trainer'])
     
 if __name__ == '__main__':
     torch.set_float32_matmul_precision('high')
-    
-    if len(sys.argv) == 2:
-        run_experiment(config_fp=sys.argv[1])
-    elif len(sys.argv) == 4:
-        run_experiment(config_fp=sys.argv[1], lr=float(sys.argv[2]), weight_decay=float(sys.argv[3]))
-    elif len(sys.argv) == 5:
-        run_experiment(config_fp=sys.argv[1], lr=float(sys.argv[2]), weight_decay=float(sys.argv[3]), channels=int(sys.argv[4]))
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_fp', type=str, required=True)
+    parser.add_argument('--lr', type=float, required=False, default=None)
+    parser.add_argument('--weight_decay', type=float, required=False, default=None)
+    parser.add_argument('--channels', type=int, required=False, default=None)
+    parser.add_argument('--layers_tuned', type=int, required=False, default=None)
+    parser.add_argument('--freeze_backbone', type=str_to_bool, required=False, default=None)
+    args = parser.parse_args()
+
+    run_experiment(config_fp=args.config_fp, lr=args.lr, weight_decay=args.weight_decay, channels=args.channels, layers_tuned=args.layers_tuned, freeze_backbone=args.freeze_backbone)
