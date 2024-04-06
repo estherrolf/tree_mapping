3917d79d573eb0c9628a22612d4db56919a88176
Lucia Gordon <luciagordon@g.harvard.edu>
2024-04-06 12:42:14 -0400

    Replaced single rasterize file with two separete files for lines and polygons

---
+++ rasterize_line.py
@@ -0,0 +1,61 @@
+# imports
+from rasterio import features
+from utils import get_project_dir
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import rasterio
+
+data_dir = f'{get_project_dir()}/data'
+
+def rasterize(vector_path, resolution): # resolution = number of meters covered by each pixel in the TIFF
+    '''Converts a vector to a raster at a specified resolution'''
+
+    feature = vector_path.split('/')[-1].split('_')[0]
+    raster_path = f'{data_dir}/features/{feature}/{feature}_raster_{resolution}m.tif'
+
+    # rasterize shapefile
+    vector = gpd.read_file(vector_path)
+    bounds = vector.total_bounds
+    width = int((bounds[2] - bounds[0]) / resolution)
+    height = int((bounds[3] - bounds[1]) / resolution)
+    transform = rasterio.transform.from_origin(bounds[0], bounds[3], resolution, resolution)
+
+    with rasterio.open(raster_path,
+                       'w',
+                       driver='GTiff',
+                       height=height,
+                       width=width,
+                       count=1,
+                       dtype=rasterio.uint8,
+                       crs=vector.crs,
+                       transform=transform) as dst:
+
+        burned = features.rasterize(((geometry, 255) for geometry in vector.geometry),
+                                     out_shape=(height, width),
+                                     transform=transform,
+                                     fill=0,
+                                     all_touched=True,
+                                     dtype=rasterio.uint8) # burn the features into the raster
+
+        dst.write_band(1, burned) # write the rasterized shapefile to the GeoTIFF
+
+    # convert raster to array
+    array = rasterio.open(raster_path).read(1) # 0 = not feature, 255 = feature
+    print(f'Array shape = {array.shape}')
+    print(f'Min array = {np.amin(array)}, max array = {np.amax(array)}')
+
+    # plot raster
+    plt.figure(dpi=300)
+    plt.imshow(array) # plot the array of pixel values as an image
+    plt.axis('off') # remove axes
+    os.makedirs(f'figures/{feature}', exist_ok=True)
+    plt.savefig(f'figures/{feature}/{feature}_raster_{resolution}m.png', bbox_inches='tight', pad_inches=0)
+    plt.close() # close the image to save memory
+
+    print(f'Rasterized at {resolution}m resolution')
+
+if __name__ == '__main__':
+    rasterize(vector_path=f'{data_dir}/features/river/river_shapefile/river_shapefile.shp', resolution=10)
+    rasterize(vector_path=f'{data_dir}/features/river/river_shapefile/river_shapefile.shp', resolution=30)
