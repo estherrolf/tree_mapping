9e56102a64397eba609bdede8697bc01bd0026b6
Lucia Gordon <luciagordon@g.harvard.edu>
2024-04-12 21:26:45 -0400

    Added comment

--- generate_nn_predictions.py
+++ generate_nn_predictions.py
@@ -83,7 +83,7 @@ def generate_nn_predictions(setting_dir, subsetted_train_sites=False):
 
             for seed in range(10):
                 for train_site_count in [3, 6, 9, 12]:
-                    best_run = os.listdir(f'{project_dir}/experiment_results/{setting_dir}/{train_site_count}_train_sites/seed_{seed}/split_{split}/models')[0]
+                    best_run = os.listdir(f'{project_dir}/experiment_results/{setting_dir}/{train_site_count}_train_sites/seed_{seed}/split_{split}/models')[0] # only one hp combo since we already picked the best one
 
                     output_dir = f'{project_dir}/model_output/{setting_dir}/{train_site_count}_train_sites/seed_{seed}'
                     os.makedirs(output_dir, exist_ok=True)
@@ -92,7 +92,6 @@ def generate_nn_predictions(setting_dir, subsetted_train_sites=False):
                                                             best_run=best_run,
                                                             test_sites=test_sites,
                                                             output_dir=output_dir)
-
 
 if __name__ == '__main__':
     setting_dir = sys.argv[1] # e.g., "local_only_models/128_filters/3_channels"
